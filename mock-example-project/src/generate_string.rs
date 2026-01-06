a63ded297f2706d92a74a373220b580d87baacb2
jakob-rzeppa <rzeppa.jakob@gmail.com>
2026-01-06 14:05:07 +0100

    implement basic example without mocking

---
+++ mock-example-project/src/generate_string.rs
@@ -0,0 +1,38 @@
+pub fn generate_string_from_digit(digit: u8) -> Result<String, String> {
+    match digit {
+        0 => Ok("Zero".to_string()),
+        1 => Ok("One".to_string()),
+        2 => Ok("Two".to_string()),
+        3 => Ok("Three".to_string()),
+        4 => Ok("Four".to_string()),
+        5 => Ok("Five".to_string()),
+        6 => Ok("Six".to_string()),
+        7 => Ok("Seven".to_string()),
+        8 => Ok("Eight".to_string()),
+        9 => Ok("Nine".to_string()),
+        _ => Err("Digit should be between 0 and 9".to_string())
+    }
+}
+
+// Mocks do not interfere with the tests of the mocked function
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one() {
+        assert_eq!(generate_string_from_digit(1).unwrap(), String::from("One"));
+    }
+
+    #[test]
+    fn two() {
+        assert_eq!(generate_string_from_digit(2).unwrap(), String::from("Two"));
+    }
+
+    // etc.
+
+    #[test]
+    fn invalid_number() {
+        assert_eq!(generate_string_from_digit(10), Err("Digit should be between 0 and 9".to_string()));
+    }
+}
--- mock-example-project/src/main.rs
+++ mock-example-project/src/main.rs
@@ -1,3 +1,31 @@
+use crate::generate_string::generate_string_from_digit;
+
+mod generate_string;
+
+fn generate_output(digit: u8) -> Result<String, String> {
+    Ok(format!("Output: {}", generate_string_from_digit(digit)?))
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", generate_output(2).unwrap());
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::generate_output;
+
+    #[test]
+    fn it_works() {
+        let res = generate_output(2);
+
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), "Output: Two");
+    }
+
+    #[test]
+    fn it_propagates_the_error() {
+        let res = generate_output(10);
+
+        assert_eq!(res, Err("Digit should be between 0 and 9".to_string()));
+    }
 }
