a2c1e6b01c30edab941bc9ace8db8dac4c1ac102
jakob-rzeppa <rzeppa.jakob@gmail.com>
2026-01-06 16:01:32 +0100

    create function mock struct with basic implementations

---
+++ mock-lib/src/function_mock.rs
@@ -0,0 +1,70 @@
+use std::fmt::Debug;
+
+pub struct FunctionMock<Function, Params, Result>
+where
+    Params: Clone + PartialEq + Debug,
+    Function: Fn(Params) -> Result,
+{
+    name: String,
+    implementation: Option<Function>,
+    calls: Vec<Params>
+}
+
+impl<Function, Params, Result> FunctionMock<Function, Params, Result>
+where
+    Params: Clone + PartialEq + Debug,
+    Function: Fn(Params) -> Result,
+{
+    pub fn new(function_name: &str) -> Self {
+        Self {
+            name: function_name.to_string(),
+            implementation: None,
+            calls: Vec::new(),
+        }
+    }
+
+    // --- Mocking ---
+
+    pub fn get_implementation(&self) -> &Option<Function> {
+        &self.implementation
+    }
+
+    pub fn mock_implementation(&mut self, new_f: Function) {
+        self.implementation = Some(new_f);
+    }
+
+    pub fn clear_mock(&mut self) {
+        self.implementation = None;
+        self.calls = Vec::new();
+    }
+
+    // --- Execute ---
+
+    pub fn call(&mut self, params: Params) -> Result {
+        let implementation = self.implementation.as_ref()
+            .expect(format!("{} mock not initialized", self.name).as_str());
+
+        self.calls.push(params.clone());
+        implementation(params)
+    }
+
+    // --- Assert ---
+
+    pub fn assert_times(&self, expected_num_of_calls: u32) {
+        assert_eq!(self.calls.len(), expected_num_of_calls as usize,
+                   "Expected {} mock to be called {} times, received {}",
+                   self.name, self.calls.len(), expected_num_of_calls);
+    }
+
+    pub fn assert_with(&self, params: &Params) {
+        let mut was_called_with = false;
+
+        for called_params in self.calls.iter() {
+            if called_params == params {
+                was_called_with = true;
+            }
+        }
+
+        assert!(was_called_with, "Expected {} mock to be called with {:?}", self.name, params);
+    }
+}
