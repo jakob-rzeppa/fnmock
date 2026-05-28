08051a67c59ef7c355a7d1f81c7c79849b4a99ca
jakob-rzeppa <rzeppa.jakob@gmail.com>
2026-05-28 20:51:49 +0200

    remove unnecessary and outdated inline mock example

--- fnmock-example-project/src/main.rs
+++ fnmock-example-project/src/main.rs
@@ -1,6 +1,5 @@
 mod basic_mock;
 mod mock_and_fake;
-mod inline_mock;
 mod basic_stub;
 mod async_fake;
 mod async_stub;
@@ -10,33 +9,33 @@ mod no_macros;
 
 fn main() {
     println!("=== fnmock Example Project ===");
-    
+
     // Call example functions to avoid unused warnings
     let _ = basic_mock::db::fetch_user(1);
     basic_mock::handle_user(1);
-    
+
     let _ = mock_and_fake::db::fetch_user(1);
     let _ = mock_and_fake::db::fetch_notes(1);
     let _ = mock_and_fake::handle_user(1);
-    
-    let _ = inline_mock::fetch_user(1);
-    inline_mock::handle_user(1);
-    
+
     let _ = basic_stub::config::get_config();
     let _ = basic_stub::process_config();
-    
+
     // Async functions
-    let _ = tokio::runtime::Runtime::new().unwrap().block_on(async {
-        let _ = async_fake::db::fetch_user(1).await;
-        let _ = async_fake::handle_user(1).await;
-        
-        let _ = async_stub::config::get_config(1).await;
-        let _ = async_stub::process_config(1).await;
-        
-        let _ = async_mock::db::fetch_user(1).await;
-        async_mock::handle_user(1).await;
-    });
-    
+    let _ = tokio::runtime::Runtime
+        ::new()
+        .unwrap()
+        .block_on(async {
+            let _ = async_fake::db::fetch_user(1).await;
+            let _ = async_fake::handle_user(1).await;
+
+            let _ = async_stub::config::get_config(1).await;
+            let _ = async_stub::process_config(1).await;
+
+            let _ = async_mock::db::fetch_user(1).await;
+            async_mock::handle_user(1).await;
+        });
+
     let _ = ignore_mock::db::save_user(1, "test".to_string(), 0);
     let _ = ignore_mock::db::update_record(1, "test".to_string(), &[1, 2], 0);
     let _ = ignore_mock::db::delete_user(1);
