bc057f8fee9bf5009257b65f306a29d2d696dce2
jakob-rzeppa <rzeppa.jakob@gmail.com>
2026-05-29 12:28:38 +0200

    add fake generic example files empty

--- fnmock-example-project/src/function/mod.rs
+++ fnmock-example-project/src/function/mod.rs
@@ -1,2 +1,4 @@
 mod fake_regular;
 mod fake_regular_async;
+mod fake_generic;
+mod fake_generic_async;
--- fnmock-example-project/src/main.rs
+++ fnmock-example-project/src/main.rs
@@ -1,46 +1,6 @@
-mod basic_mock;
-mod mock_and_fake;
-mod basic_stub;
-mod async_fake;
-mod async_stub;
-mod async_mock;
-mod ignore_mock;
-mod no_macros;
 #[allow(dead_code)]
 mod function;
 
 fn main() {
     println!("=== fnmock Example Project ===");
-
-    // Call example functions to avoid unused warnings
-    let _ = basic_mock::db::fetch_user(1);
-    basic_mock::handle_user(1);
-
-    let _ = mock_and_fake::db::fetch_user(1);
-    let _ = mock_and_fake::db::fetch_notes(1);
-    let _ = mock_and_fake::handle_user(1);
-
-    let _ = basic_stub::config::get_config();
-    let _ = basic_stub::process_config();
-
-    // Async functions
-    let _ = tokio::runtime::Runtime
-        ::new()
-        .unwrap()
-        .block_on(async {
-            let _ = async_fake::db::fetch_user(1).await;
-            let _ = async_fake::handle_user(1).await;
-
-            let _ = async_stub::config::get_config(1).await;
-            let _ = async_stub::process_config(1).await;
-
-            let _ = async_mock::db::fetch_user(1).await;
-            async_mock::handle_user(1).await;
-        });
-
-    let _ = ignore_mock::db::save_user(1, "test".to_string(), 0);
-    let _ = ignore_mock::db::update_record(1, "test".to_string(), &[1, 2], 0);
-    let _ = ignore_mock::db::delete_user(1);
-
-    let _ = no_macros::basic_mock::db::fetch_user(1);
 }
