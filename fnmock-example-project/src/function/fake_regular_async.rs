da16910a774aa5092a0c469be2a5d01e7b48109b
jakob-rzeppa <rzeppa.jakob@gmail.com>
2026-06-02 21:19:48 +0200

    implement async fake function examples

--- fnmock-example-project/src/function/fake_regular_async.rs
+++ fnmock-example-project/src/function/fake_regular_async.rs
@@ -1,10 +1,47 @@
 async fn get_user(id: i32) -> String {
+    if get_user_fake::is_set() {
+        let impl_fn = get_user_fake::get();
+        return impl_fn(id);
+    }
+
     format!("User {}", id)
 }
 
+pub(crate) mod get_user_fake {
+    use std::rc::Rc;
+
+    use fnmock::{ fake_store::FakeStore };
+
+    thread_local! {
+        static FAKE: std::cell::RefCell<FakeStore<fn(i32) -> String>> = std::cell::RefCell::new(
+            FakeStore::new("get_user")
+        );
+    }
+
+    pub(crate) fn setup(function: fn(i32) -> String) {
+        FAKE.with_borrow_mut(|fake| {
+            fake.setup(function);
+        });
+    }
+
+    pub(crate) fn clear() {
+        FAKE.with_borrow_mut(|fake| {
+            fake.clear();
+        })
+    }
+
+    pub(crate) fn is_set() -> bool {
+        FAKE.with_borrow(|fake| { fake.is_set() })
+    }
+
+    pub(crate) fn get() -> Rc<fn(i32) -> String> {
+        FAKE.with_borrow(|fake| { fake.get() })
+    }
+}
+
 async fn handle_user(id: i32) -> String {
-    let user = get_user(id).await;
-    user
+    let user = get_user(id);
+    user.await
 }
 
 #[cfg(test)]
@@ -20,6 +57,9 @@ mod tests {
 
     #[tokio::test]
     async fn test_handle_user_with_fake() {
+        // The closure must be not async. The result will be wrapped in a future by the `get_user` function.
+        get_user_fake::setup(|id| format!("Fake User {}", id));
+
         let result = handle_user(1).await;
 
         assert_eq!(result, "Fake User 1");
