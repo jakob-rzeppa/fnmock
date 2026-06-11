026a03030e8e079277ae3492da9d0491b09202b9
jakob-rzeppa <rzeppa.jakob@gmail.com>
2026-06-11 10:54:48 +0200

    rename spy functions and add additional functionality

--- fnmock-example-project/src/function/spy_regular.rs
+++ fnmock-example-project/src/function/spy_regular.rs
@@ -10,49 +10,73 @@ pub(crate) mod get_user_spy {
     use fnmock::spy::Spy;
 
     thread_local! {
-        static FAKE: std::cell::RefCell<Spy<i32>> = std::cell::RefCell::new(Spy::new("get_user"));
+        static GET_USER_SPY: std::cell::RefCell<Spy<i32>> = std::cell::RefCell::new(
+            Spy::new("get_user")
+        );
+    }
+
+    pub(crate) fn setup() {
+        GET_USER_SPY.with_borrow_mut(|spy| {
+            spy.setup();
+        });
+    }
+
+    pub(crate) fn teardown() {
+        GET_USER_SPY.with_borrow_mut(|spy| {
+            spy.teardown();
+        });
     }
 
     pub(crate) fn save(args: i32) {
-        FAKE.with_borrow_mut(|spy| {
+        GET_USER_SPY.with_borrow_mut(|spy| {
             spy.save(args);
         });
     }
 
     pub(crate) fn clear() {
-        FAKE.with_borrow_mut(|spy| {
+        GET_USER_SPY.with_borrow_mut(|spy| {
             spy.clear();
         })
     }
 
-    pub(crate) fn assert_times(times: usize) {
-        FAKE.with_borrow(|spy| {
-            spy.assert_times(times);
-        })
+    pub(crate) fn called_times(times: usize) -> bool {
+        GET_USER_SPY.with_borrow(|spy| { spy.called_times(times) })
     }
 
-    pub(crate) fn assert_any(args: fn(i32) -> bool) {
-        FAKE.with_borrow(|spy| {
-            spy.assert_any(args);
-        })
+    pub(crate) fn assert_called_times(times: usize) {
+        GET_USER_SPY.with_borrow(|spy| { assert!(spy.called_times(times)) })
     }
 
-    pub(crate) fn assert_any_with(args: i32) {
-        FAKE.with_borrow(|spy| {
-            spy.assert_any_with(args);
-        })
+    pub(crate) fn any_call_matches(args: fn(i32) -> bool) -> bool {
+        GET_USER_SPY.with_borrow(|spy| { spy.any_call_matches(args) })
     }
 
-    pub(crate) fn assert_nth(n: usize, args: fn(i32) -> bool) {
-        FAKE.with_borrow(|spy| {
-            spy.assert_nth(n, args);
-        })
+    pub(crate) fn assert_any_call_matches(args: fn(i32) -> bool) {
+        GET_USER_SPY.with_borrow(|spy| { assert!(spy.any_call_matches(args)) })
     }
 
-    pub(crate) fn assert_nth_with(n: usize, args: i32) {
-        FAKE.with_borrow(|spy| {
-            spy.assert_nth_with(n, args);
-        })
+    pub(crate) fn any_call_equals(args: i32) -> bool {
+        GET_USER_SPY.with_borrow(|spy| { spy.any_call_equals(args) })
+    }
+
+    pub(crate) fn assert_any_call_equals(args: i32) {
+        GET_USER_SPY.with_borrow(|spy| { assert!(spy.any_call_equals(args)) })
+    }
+
+    pub(crate) fn nth_call_matches(n: usize, args: fn(i32) -> bool) -> bool {
+        GET_USER_SPY.with_borrow(|spy| { spy.nth_call_matches(n, args) })
+    }
+
+    pub(crate) fn assert_nth_call_matches(n: usize, args: fn(i32) -> bool) {
+        GET_USER_SPY.with_borrow(|spy| { assert!(spy.nth_call_matches(n, args)) })
+    }
+
+    pub(crate) fn nth_call_equals(n: usize, args: i32) -> bool {
+        GET_USER_SPY.with_borrow(|spy| { spy.nth_call_equals(n, args) })
+    }
+
+    pub(crate) fn assert_nth_call_equals(n: usize, args: i32) {
+        GET_USER_SPY.with_borrow(|spy| { assert!(spy.nth_call_equals(n, args)) })
     }
 }
 
@@ -67,15 +91,18 @@ mod tests {
 
     #[test]
     fn test_handle_user_spy() {
+        get_user_spy::setup();
+
         let result = handle_user(1);
 
         assert_eq!(result, "User 1");
 
-        get_user_spy::assert_times(1);
-        get_user_spy::assert_any(|id| id == 1);
-        get_user_spy::assert_any_with(1);
-        get_user_spy::assert_nth(0, |id| id == 1);
-        get_user_spy::assert_nth_with(0, 1);
+        assert!(get_user_spy::called_times(1));
+        get_user_spy::assert_called_times(1);
+        get_user_spy::assert_any_call_matches(|id| id == 1);
+        get_user_spy::assert_any_call_equals(1);
+        get_user_spy::assert_nth_call_matches(0, |id| id == 1);
+        get_user_spy::assert_nth_call_equals(0, 1);
 
         get_user_spy::clear();
 
@@ -83,8 +110,8 @@ mod tests {
 
         assert_eq!(result, "User 2");
 
-        get_user_spy::assert_times(1);
-        get_user_spy::assert_any_with(2);
-        get_user_spy::assert_nth_with(0, 2);
+        get_user_spy::assert_called_times(1);
+        get_user_spy::assert_any_call_matches(|id| id == 2);
+        get_user_spy::assert_nth_call_matches(0, |id| id == 2);
     }
 }
--- fnmock/src/spy.rs
+++ fnmock/src/spy.rs
@@ -3,49 +3,102 @@ pub struct IgnoredParam;
 
 pub struct Spy<Args: Clone + PartialEq> {
     name: &'static str,
-    calls: Vec<Args>,
+    calls: Option<Vec<Args>>,
 }
 
 impl<Args: Clone + PartialEq> Spy<Args> {
     pub fn new(name: &'static str) -> Self {
         Self {
             name,
-            calls: Vec::new(),
+            calls: None,
         }
     }
 
+    /// Sets up the spy to start recording calls. If the spy is already set up, this will panic.
+    ///
+    /// If you want to reset the spy, call clear() instead of setup() a second time.
+    ///
+    /// This method is separate from the constructor to allow for more flexible test setup.
+    /// You can choose to set up the spy at the beginning of your tests, or only in specific test sections where you want to record calls.
+    pub fn setup(&mut self) {
+        if let Some(_) = &self.calls {
+            panic!("{} spy already initialized. If you want to reset it, call clear().", self.name);
+        }
+
+        self.calls = Some(Vec::new());
+    }
+
+    /// Tears down the spy, clearing all recorded calls and preventing further calls from being recorded until setup() is called again.
+    ///
+    /// This method is separate from clear() to allow for more flexible test teardown.
+    /// After tearing down the spy, it will not record any calls until setup() is called again,
+    /// and any attempt to assert on calls will panic until the spy is set up again.
+    pub fn teardown(&mut self) {
+        self.calls = None;
+    }
+
+    /// Saves the arguments of a call to the spy. If the spy is not set up, this will not do anything.
     pub fn save(&mut self, args: Args) {
-        self.calls.push(args);
+        if let Some(calls) = &mut self.calls {
+            calls.push(args);
+        }
     }
 
+    /// Clears all recorded calls from the spy. If the spy is not set up, this will panic.
     pub fn clear(&mut self) {
-        self.calls.clear();
+        if let Some(calls) = &mut self.calls {
+            calls.clear();
+        } else {
+            panic!("clear can't be executed on {}. Spy not initialized.", self.name)
+        }
     }
 
-    pub fn assert_times(&self, times: usize) {
-        assert_eq!(
-            self.calls.len(),
-            times,
-            "Expected '{}' to be called {} times, but it was called {} times.",
-            self.name,
-            times,
-            self.calls.len()
-        );
+    /// Returns true if the spy was called the specified number of times. If the spy is not set up, this will panic.
+    pub fn called_times(&self, times: usize) -> bool {
+        if let Some(calls) = &self.calls {
+            calls.len() == times
+        } else {
+            panic!("called_times can't be executed on {}. Spy not initialized.", self.name)
+        }
     }
 
-    pub fn assert_any(&self, args: fn(Args) -> bool) {
-        assert!(self.calls.iter().any(|call_args| args(call_args.clone())));
+    /// Returns true if any call to the spy matches the provided arguments according to the provided matching function.
+    /// If the spy is not set up, this will panic.
+    pub fn any_call_matches(&self, args: fn(Args) -> bool) -> bool {
+        if let Some(calls) = &self.calls {
+            calls.iter().any(|call_args| args(call_args.clone()))
+        } else {
+            panic!("any_call_matches can't be executed on {}. Spy not initialized.", self.name)
+        }
     }
 
-    pub fn assert_any_with(&self, args: Args) {
-        assert!(self.calls.iter().any(|call_args| *call_args == args));
+    /// Returns true if any call to the spy equals the provided arguments.
+    /// If the spy is not set up, this will panic.
+    pub fn any_call_equals(&self, args: Args) -> bool {
+        if let Some(calls) = &self.calls {
+            calls.iter().any(|call_args| *call_args == args)
+        } else {
+            panic!("any_call_equals can't be executed on {}. Spy not initialized.", self.name)
+        }
     }
 
-    pub fn assert_nth(&self, n: usize, args: fn(Args) -> bool) {
-        assert!(self.calls.get(n).map_or(false, |call_args| args(call_args.clone())));
+    /// Returns true if the nth call to the spy matches the provided arguments according to the provided matching function.
+    /// If the spy is not set up, this will panic.
+    pub fn nth_call_matches(&self, n: usize, args: fn(Args) -> bool) -> bool {
+        if let Some(calls) = &self.calls {
+            calls.get(n).map_or(false, |call_args| args(call_args.clone()))
+        } else {
+            panic!("nth_call_matches can't be executed on {}. Spy not initialized.", self.name)
+        }
     }
 
-    pub fn assert_nth_with(&self, n: usize, args: Args) {
-        assert!(self.calls.get(n).map_or(false, |call_args| *call_args == args));
+    /// Returns true if the nth call to the spy equals the provided arguments.
+    /// If the spy is not set up, this will panic.
+    pub fn nth_call_equals(&self, n: usize, args: Args) -> bool {
+        if let Some(calls) = &self.calls {
+            calls.get(n).map_or(false, |call_args| *call_args == args)
+        } else {
+            panic!("nth_call_equals can't be executed on {}. Spy not initialized.", self.name)
+        }
     }
 }
