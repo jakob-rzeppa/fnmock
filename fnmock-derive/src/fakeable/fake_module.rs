6d27c47f9ca6248ed36edd02c515460784e01eef
jakob-rzeppa <rzeppa.jakob@gmail.com>
2026-06-15 23:55:03 +0200

    add cfg(test) before fakeable modules

--- fnmock-derive/src/fakeable/fake_module.rs
+++ fnmock-derive/src/fakeable/fake_module.rs
@@ -19,45 +19,46 @@ fn generate_regular_fake_module_code(info: &FakeableInfo) -> syn::Result<syn::It
 
     let code =
         quote! {
-        mod #module_name {
-            use super::*;
+            #[cfg(test)]
+            mod #module_name {
+                use super::*;
 
-            thread_local! {
-                static #store_name: std::cell::RefCell<fnmock::fake_store::FakeStore<#fn_ptr_type>> =
-                    std::cell::RefCell::new(fnmock::fake_store::FakeStore::new(stringify!(#display_name)));
+                thread_local! {
+                    static #store_name: std::cell::RefCell<fnmock::fake_store::FakeStore<#fn_ptr_type>> =
+                        std::cell::RefCell::new(fnmock::fake_store::FakeStore::new(stringify!(#display_name)));
+                }
+
+                pub(crate) struct #interface_struct_name;
+
+                impl #interface_struct_name {
+                    pub(crate) fn new() -> Self {
+                        Self
+                    }
+
+                    pub(crate) fn setup(self, function: #fn_ptr_type) -> Self {
+                        #store_name.with(|store| {
+                            store.borrow_mut().setup(function);
+                        });
+                        self
+                    }
+
+                    pub(crate) fn clear(self) -> Self {
+                        #store_name.with(|store| {
+                            store.borrow_mut().clear();
+                        });
+                        self
+                    }
+
+                    pub(crate) fn is_set(&self) -> bool {
+                        #store_name.with(|store| store.borrow().is_set())
+                    }
+
+                    pub(crate) fn get(&self) -> std::rc::Rc<#fn_ptr_type> {
+                        #store_name.with(|store| store.borrow().get())
+                    }
+                }
             }
-
-            pub(crate) struct #interface_struct_name;
-
-            impl #interface_struct_name {
-                pub(crate) fn new() -> Self {
-                    Self
-                }
-
-                pub(crate) fn setup(self, function: #fn_ptr_type) -> Self {
-                    #store_name.with(|store| {
-                        store.borrow_mut().setup(function);
-                    });
-                    self
-                }
-
-                pub(crate) fn clear(self) -> Self {
-                    #store_name.with(|store| {
-                        store.borrow_mut().clear();
-                    });
-                    self
-                }
-
-                pub(crate) fn is_set(&self) -> bool {
-                    #store_name.with(|store| store.borrow().is_set())
-                }
-
-                pub(crate) fn get(&self) -> std::rc::Rc<#fn_ptr_type> {
-                    #store_name.with(|store| store.borrow().get())
-                }
-            }
-        }
-    };
+        };
 
     syn::parse2(code)
 }
@@ -86,55 +87,56 @@ fn generate_generic_fake_module_code(info: &FakeableInfo) -> syn::Result<syn::It
 
     let code =
         quote! {
-        mod #module_name {
-            use super::*;
+            #[cfg(test)]
+            mod #module_name {
+                use super::*;
 
-        thread_local! {
-            static #store_name: std::cell::RefCell<fnmock::generic_fake_store::GenericFakeStore<#generic_count>> = 
-                std::cell::RefCell::new(
-                    fnmock::generic_fake_store::GenericFakeStore::new(stringify!(#display_name))
-                );
-        }
+                thread_local! {
+                    static #store_name: std::cell::RefCell<fnmock::generic_fake_store::GenericFakeStore<#generic_count>> = 
+                        std::cell::RefCell::new(
+                            fnmock::generic_fake_store::GenericFakeStore::new(stringify!(#display_name))
+                        );
+                }
 
-        pub(crate) struct #interface_struct_name<#(#generic_params),*> {
-            _marker: std::marker::PhantomData<(#(#generic_idents),*)>,
-        }
+                pub(crate) struct #interface_struct_name<#(#generic_params),*> {
+                    _marker: std::marker::PhantomData<(#(#generic_idents),*)>,
+                }
 
-        impl<#(#generic_params),*> #interface_struct_name<#(#generic_idents),*> {
-            pub(crate) fn new() -> Self {
-                Self {
-                    _marker: std::marker::PhantomData,
+                impl<#(#generic_params),*> #interface_struct_name<#(#generic_idents),*> {
+                    pub(crate) fn new() -> Self {
+                        Self {
+                            _marker: std::marker::PhantomData,
+                        }
+                    }
+
+                    pub(crate) fn setup(self, function: #fn_ptr_type) -> Self {
+                        #store_name.with_borrow_mut(|fake| {
+                            fake.setup_for([#(#generic_type_ids),*], function);
+                        });
+                        self
+                    }
+
+                    pub(crate) fn clear(self) -> Self {
+                        #store_name.with_borrow_mut(|fake| {
+                            fake.clear_for([#(#generic_type_ids),*]);
+                        });
+                        self
+                    }
+
+                    pub(crate) fn is_set(&self) -> bool {
+                        #store_name.with_borrow(|fake| {
+                            fake.is_set_for([#(#generic_type_ids),*])
+                        })
+                    }
+
+                    pub(crate) fn get(&self) -> std::rc::Rc<#fn_ptr_type> {
+                        #store_name.with_borrow(|fake| {
+                            fake.get_for::<#fn_ptr_type>([#(#generic_type_ids),*])
+                        })
+                    }
                 }
             }
-
-            pub(crate) fn setup(self, function: #fn_ptr_type) -> Self {
-                #store_name.with_borrow_mut(|fake| {
-                    fake.setup_for([#(#generic_type_ids),*], function);
-                });
-                self
-            }
-
-            pub(crate) fn clear(self) -> Self {
-                #store_name.with_borrow_mut(|fake| {
-                    fake.clear_for([#(#generic_type_ids),*]);
-                });
-                self
-            }
-
-            pub(crate) fn is_set(&self) -> bool {
-                #store_name.with_borrow(|fake| {
-                    fake.is_set_for([#(#generic_type_ids),*])
-                })
-            }
-
-            pub(crate) fn get(&self) -> std::rc::Rc<#fn_ptr_type> {
-                #store_name.with_borrow(|fake| {
-                    fake.get_for::<#fn_ptr_type>([#(#generic_type_ids),*])
-                })
-            }
-        }
-        }
-    };
+        };
 
     syn::parse2(code)
 }
