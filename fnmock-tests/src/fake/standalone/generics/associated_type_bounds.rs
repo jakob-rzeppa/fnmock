ad9d5d4f4600c511af9403b499744e673d923653
jakob-rzeppa <rzeppa.jakob@gmail.com>
2026-07-09 20:09:57 +0200

    declare associated_type_bounds as not implemented and fix test for associated_type_equality

--- fnmock-tests/src/fake/standalone/generics/associated_type_bounds.rs
+++ fnmock-tests/src/fake/standalone/generics/associated_type_bounds.rs
@@ -1,22 +1,10 @@
-// use std::fmt::Display;
-
-// #[fnmock::fakeable]
-// fn associated_type_bounds<I>(value: I) -> Vec<String> where I: Iterator, I::Item: Display {
-//     value.map(|item| item.to_string()).collect()
-// }
-
-// #[test]
-// fn test_associated_type_bounds() {
-//     let result = associated_type_bounds(vec!["Test".to_string()].into_iter());
-//     assert_eq!(result, vec!["Test".to_string()]);
-// }
-
-// #[test]
-// fn test_associated_type_bounds_fake() {
-//     associated_type_bounds_fake::<std::vec::IntoIter<String>>().setup(|_value| {
-//         vec!["Fake".to_string()]
-//     });
-
-//     let result = associated_type_bounds(vec!["Test".to_string()].into_iter());
-//     assert_eq!(result, vec!["Fake".to_string()]);
-// }
+//! Associated type bounds are not supported. If possible you should use associated type equality instead.
+//!
+//! This is not supported:
+//!
+//! ```rust
+//! #[fnmock::fakeable]
+//! fn associated_type_bounds<I>(value: I) -> Vec<String> where I: Iterator + 'static, I::Item: Display {
+//!    value.map(|item| item.to_string()).collect()
+//! }
+//!```
