62d33916709183566256a7aac6dcc6873c332c92
jakob-rzeppa <rzeppa.jakob@gmail.com>
2026-07-09 20:22:03 +0200

    fix test for higher_ranked_bounds

--- fnmock-tests/src/fake/standalone/generics/higher_ranked_bounds.rs
+++ fnmock-tests/src/fake/standalone/generics/higher_ranked_bounds.rs
@@ -1,18 +1,58 @@
-// #[fnmock::fakeable]
-// fn higher_ranked_bounds<F>(value: F) -> String where F: for<'a> Fn(&'a str) -> &'a str {
-//     value("Real").to_string()
-// }
+use std::fmt::Debug;
 
-// #[test]
-// fn test_higher_ranked_bounds() {
-//     let result = higher_ranked_bounds(|value| value);
-//     assert_eq!(result, "Real");
-// }
+#[fnmock::fakeable]
+fn higher_ranked_bounds_closure<F>(value: F) -> String
+    where F: for<'a> Fn(&'a str) -> &'a str + 'static
+{
+    value("Real").to_string()
+}
 
-// #[test]
-// fn test_higher_ranked_bounds_fake() {
-//     higher_ranked_bounds_fake().setup(|_value| "Fake".to_string());
+#[test]
+fn test_higher_ranked_bounds_closure() {
+    let result = higher_ranked_bounds_closure::<for<'a> fn(&'a str) -> &'a str>(|value| value);
+    assert_eq!(result, "Real");
+}
 
-//     let result = higher_ranked_bounds(|value| value);
-//     assert_eq!(result, "Fake");
-// }
+#[test]
+fn test_higher_ranked_bounds_closure_fake() {
+    higher_ranked_bounds_closure_fake::<for<'a> fn(&'a str) -> &'a str>().setup(|_value|
+        "Fake".to_string()
+    );
+
+    let result = higher_ranked_bounds_closure::<for<'a> fn(&'a str) -> &'a str>(|value| value);
+    assert_eq!(result, "Fake");
+}
+
+trait HigherRankedBoundsTrait<T>: Debug {}
+
+#[derive(Debug)]
+struct MyStruct {
+    data: String,
+}
+
+impl HigherRankedBoundsTrait<&str> for MyStruct {}
+
+#[fnmock::fakeable]
+fn higher_ranked_bounds_trait<I>(value: I) -> String
+    where I: for<'a> HigherRankedBoundsTrait<&'a str> + 'static
+{
+    format!("{:?}", value)
+}
+
+#[test]
+fn test_higher_ranked_bounds_trait() {
+    let result = higher_ranked_bounds_trait::<MyStruct>(MyStruct {
+        data: "Real".to_string(),
+    });
+    assert_eq!(result, "MyStruct { data: \"Real\" }");
+}
+
+#[test]
+fn test_higher_ranked_bounds_trait_fake() {
+    higher_ranked_bounds_trait_fake::<MyStruct>().setup(|_value| "Fake".to_string());
+
+    let result = higher_ranked_bounds_trait::<MyStruct>(MyStruct {
+        data: "Fake".to_string(),
+    });
+    assert_eq!(result, "Fake");
+}
