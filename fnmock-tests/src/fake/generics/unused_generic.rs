d1ff3293884f41d993589512e467b003008afe20
jakob-rzeppa <rzeppa.jakob@gmail.com>
2026-07-16 22:01:37 +0200

    fix some test names not changed after copying

--- fnmock-tests/src/fake/generics/unused_generic.rs
+++ fnmock-tests/src/fake/generics/unused_generic.rs
@@ -1,17 +1,17 @@
 #[fnmock::fakeable]
-fn single_generic<T: 'static, U: 'static>(a: T) -> T {
+fn unused_generic<T: 'static, U: 'static>(a: T) -> T {
     a
 }
 
 #[test]
-fn test_single_generic() {
-    let res = single_generic::<String, String>("Test".to_string());
+fn test_unused_generic() {
+    let res = unused_generic::<String, String>("Test".to_string());
     assert_eq!(res, "Test");
 }
 
 #[test]
-fn test_single_generic_fake() {
-    single_generic_fake::<String, String>().setup(|a| format!("Fake {}", a));
-    let res = single_generic::<String, String>("Test".to_string());
+fn test_unused_generic_fake() {
+    unused_generic_fake::<String, String>().setup(|a| format!("Fake {}", a));
+    let res = unused_generic::<String, String>("Test".to_string());
     assert_eq!(res, "Fake Test");
 }
