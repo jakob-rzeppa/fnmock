38b23c599e76e25dc901d39f5153417398c4d1d2
jakob-rzeppa <rzeppa.jakob@gmail.com>
2026-08-29 13:06:04 +0200

    delete old generic fake tests

--- fnmock-tests/src/fake/mod.rs
+++ fnmock-tests/src/fake/mod.rs
@@ -2,18 +2,6 @@ mod basic {
     mod clear_and_is_set;
     mod reentrant_fake;
 }
-mod generics {
-    mod associated_type_equality;
-    mod bounds_lifetime;
-    mod clear_and_is_set;
-    mod const_generics_clear_and_is_set;
-    mod higher_ranked_bounds;
-    mod named_lifetime;
-    mod return_generic;
-    mod static_lifetime;
-    mod unused_lifetime;
-}
-
 mod visibility {
     // mod impl_method_pub;
     // mod impl_method_pub_crate;
