e9f0d3bc4349d9e35b87de954c8e7c08def43860
jakob-rzeppa <rzeppa.jakob@gmail.com>
2026-08-29 11:18:34 +0200

    rename compile fail tests

--- fnmock-tests/src/main.rs
+++ fnmock-tests/src/main.rs
@@ -1,11 +1,11 @@
 #[allow(dead_code)]
 mod common;
 #[allow(dead_code)]
+mod compile_fail;
+#[allow(dead_code)]
 mod fake;
 #[allow(dead_code)]
 mod spy;
-#[allow(dead_code)]
-mod unsupported;
 
 fn main() {
     println!("=== fnmock Example Project ===");
