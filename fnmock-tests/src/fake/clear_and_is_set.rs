56640d42d688e5d855c75d3378bc4b7563c7e27c
jakob-rzeppa <rzeppa.jakob@gmail.com>
2026-09-22 12:54:55 +0200

    add mod mock arms to fake/

--- fnmock-tests/src/fake/clear_and_is_set.rs
+++ fnmock-tests/src/fake/clear_and_is_set.rs
@@ -1,35 +1,67 @@
-#[fnmock::fakeable]
-fn clear_and_is_set(a: String) -> String {
-    format!("Real {}", a)
+mod fake {
+    #[fnmock::fakeable]
+    fn clear_and_is_set(a: String) -> String {
+        format!("Real {}", a)
+    }
+
+    #[test]
+    fn test_is_set_transitions() {
+        assert!(!clear_and_is_set_fake().is_set());
+
+        clear_and_is_set_fake().setup(|a| format!("Fake {}", a));
+        assert!(clear_and_is_set_fake().is_set());
+
+        clear_and_is_set_fake().clear();
+        assert!(!clear_and_is_set_fake().is_set());
+    }
+
+    #[test]
+    fn test_clear_restores_real_implementation() {
+        clear_and_is_set_fake().setup(|a| format!("Fake {}", a));
+        let res = clear_and_is_set("Test".to_string());
+        assert_eq!(res, "Fake Test");
+
+        clear_and_is_set_fake().clear();
+        let res = clear_and_is_set("Test".to_string());
+        assert_eq!(res, "Real Test");
+    }
+
+    #[test]
+    fn test_setup_twice_overwrites() {
+        clear_and_is_set_fake().setup(|a| format!("First {}", a));
+        clear_and_is_set_fake().setup(|a| format!("Second {}", a));
+
+        let res = clear_and_is_set("Test".to_string());
+        assert_eq!(res, "Second Test");
+    }
 }
 
-#[test]
-fn test_is_set_transitions() {
-    assert!(!clear_and_is_set_fake().is_set());
+mod mock {
+    #[fnmock::mockable]
+    fn clear_and_is_set(a: String) -> String {
+        format!("Real {}", a)
+    }
 
-    clear_and_is_set_fake().setup(|a| format!("Fake {}", a));
-    assert!(clear_and_is_set_fake().is_set());
+    #[test]
+    fn test_is_set_transitions() {
+        assert!(!clear_and_is_set_mock().is_set());
 
-    clear_and_is_set_fake().clear();
-    assert!(!clear_and_is_set_fake().is_set());
-}
+        clear_and_is_set_mock().setup(|a| format!("Fake {}", a));
+        assert!(clear_and_is_set_mock().is_set());
 
-#[test]
-fn test_clear_restores_real_implementation() {
-    clear_and_is_set_fake().setup(|a| format!("Fake {}", a));
-    let res = clear_and_is_set("Test".to_string());
-    assert_eq!(res, "Fake Test");
+        clear_and_is_set_mock().clear();
+        assert!(!clear_and_is_set_mock().is_set());
+    }
 
-    clear_and_is_set_fake().clear();
-    let res = clear_and_is_set("Test".to_string());
-    assert_eq!(res, "Real Test");
-}
+    #[test]
+    fn test_setup_and_expect_together() {
+        let mock = clear_and_is_set_mock();
+        mock.setup(|a| format!("Fake {}", a));
+        mock.expect(fnmock::predicate::eq("Test".to_string())).once();
 
-#[test]
-fn test_setup_twice_overwrites() {
-    clear_and_is_set_fake().setup(|a| format!("First {}", a));
-    clear_and_is_set_fake().setup(|a| format!("Second {}", a));
+        let res = clear_and_is_set("Test".to_string());
 
-    let res = clear_and_is_set("Test".to_string());
-    assert_eq!(res, "Second Test");
+        assert_eq!(res, "Fake Test");
+        mock.assert();
+    }
 }
